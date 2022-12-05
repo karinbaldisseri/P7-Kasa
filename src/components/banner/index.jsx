0f8f0f96cb6a2d86273caf82d210874ac5a0ed97
Karin Baldisseri <karinbaldi@hotmail.com>
2022-12-05 10:22:02 +0100

    Feature Collapse component

--- src/components/banner/index.jsx
+++ src/components/banner/index.jsx
@@ -1,14 +1,5 @@
-import "./banner.scss";
 import PropTypes from "prop-types";
-
-/* export default function Banner(props) {
-  return (
-    <section className="bannerContainer">
-      <img src={props.img} alt={props.alt}/>
-      <h2>{props.title}</h2>
-    </section>
-  );
-} */
+import "./banner.scss";
 
 function Banner({ children }) {
   return <section className="bannerContainer">{children}</section>;
