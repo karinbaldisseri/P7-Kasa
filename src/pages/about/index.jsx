cd5c1c5ff8a90e3b1688405afcf03edc4c6c9d40
Karin Baldisseri <karinbaldi@hotmail.com>
2022-12-05 12:48:49 +0100

    Feature About page collapsesection

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -3,19 +3,43 @@ import AboutBannerImg from "../../assets/about_banner_img.png";
 import Collapse from "../../components/collapse";
 
 export default function About() {
+  const aboutData = [
+    {
+      id: "1",
+      header: "Fiabilité",
+      content:
+        "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.",
+    },
+    {
+      id: "2",
+      header: "Respect",
+      content:
+        "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.",
+    },
+    {
+      id: "3",
+      header: "Service",
+      content:
+        "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.",
+    },
+    {
+      id: "4",
+      header: "Sécurité",
+      content:
+        "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
+    },
+  ];
+
   return (
     <main>
       <Banner>
         <img src={AboutBannerImg} alt="paysage montagneux" />
       </Banner>
-      <Collapse
-        header="Title"
-        content="Collapse element text : Lorem ipsum dolor sit amet, consectetur
-          adipiscing elit. Sed eu iaculis lacus. Quisque diam nibh, finibus ac
-          neque vel, sollicitudin interdum urna. Nullam id convallis est. Proin
-          gravida sapien eu imperdiet posuere. Nullam sed massa at sapien
-          bibendum accumsan."
-      />
+      {aboutData.map((item) => {
+        return (
+          <Collapse header={item.header} content={item.content} key={item.id} />
+        );
+      })}
     </main>
   );
 }
